cf8b70707a384ae36a6067c4341189e9b551ca16
khangba153 <khangba153@gmail.com>
2026-09-06 21:10:58 +0700

    chore: configure web API client

--- TuanKietBranchFlow.Web/Program.cs
+++ TuanKietBranchFlow.Web/Program.cs
@@ -2,25 +2,41 @@ using TuanKietBranchFlow.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Đăng ký Blazor Interactive Server.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Đọc địa chỉ API từ file appsettings
+string? apiBaseUrl = builder.Configuration["ApiSettings:BaseUrl"];
+
+// Dừng úng dụng sớm nếu chưa cấu hình địa chỉ API
+if (string.IsNullOrWhiteSpace(apiBaseUrl))
+{
+    throw new InvalidOperationException("Chưa cấu hình ApiSettings:BaseUrl.");
+}
+
+// Đăng ký HttpClient dùng để gọi BranchFlow API
+builder.Services.AddHttpClient(
+    "BranchFlowApi",client =>
+    {
+        client.BaseAddress = new Uri(apiBaseUrl);
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
